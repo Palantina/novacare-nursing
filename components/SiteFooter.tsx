a9db5af73ce0f78cd8a707e28e24fa08827d4ec9
Palantina <palantina@outlook.com>
2025-09-04 14:04:46 +1000

    Update SiteFooter.tsx
    Commit changes

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,32 +1,27 @@
 import React from "react";
 
-type SiteFooterProps = {
-  hideContactInfo?: boolean;
-};
-
 const brand = { blue: "#0B2D5C" };
 
-export default function SiteFooter({ hideContactInfo = false }: SiteFooterProps) {
+export default function SiteFooter() {
   return (
     <footer className="mt-16 border-t border-gray-200 py-8 text-center text-sm opacity-80">
-      {!hideContactInfo && (
-        <div className="mb-4">
-          <p>📍 Hobart & Surrounds, Tasmania</p>
-          <p>
-            📞{" "}
-            <a href="tel:+61488086271" className="underline">
-              0488 086 271
-            </a>{" "}
-            &nbsp; | &nbsp; ✉{" "}
-            <a href="mailto:info@novacarenursing.com.au" className="underline">
-              info@novacarenursing.com.au
-            </a>
-          </p>
-        </div>
-      )}
+      <div className="mb-4">
+        <p>📍 Hobart & Surrounds, Tasmania</p>
+        <p>
+          📞{" "}
+          <a href="tel:+61488086271" className="underline">
+            0488 086 271
+          </a>{" "}
+          &nbsp; | &nbsp; ✉{" "}
+          <a href="mailto:info@novacarenursing.com.au" className="underline">
+            info@novacarenursing.com.au
+          </a>
+        </p>
+      </div>
       <p style={{ color: brand.blue }}>
         © {new Date().getFullYear()} NovaCare Nursing. All rights reserved.
       </p>
     </footer>
   );
 }
+
